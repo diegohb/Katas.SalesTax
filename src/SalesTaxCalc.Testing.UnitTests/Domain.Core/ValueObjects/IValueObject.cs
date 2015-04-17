d88ff9ddebcb5ed9507bd2da1f29ae47034b7492
bustamd1 <bustamd1@.(none)>
2015-04-17 19:56:05 -0400

    -add dir domain.core for all shared domain objects. -add marker interface for value objects

---
+++ src/SalesTaxCalc.Testing.UnitTests/Domain.Core/ValueObjects/IValueObject.cs
@@ -0,0 +1,13 @@
+﻿// *************************************************
+// SalesTaxCalc.Testing.UnitTests.IValueObject.cs
+// Last Modified: 04/17/2015 7:54 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace SalesTaxCalc.Testing.UnitTests.Domain.Core.ValueObjects
+{
+    /// <summary>
+    ///     Marker interface for all value objects.
+    /// </summary>
+    public interface IValueObject {}
+}

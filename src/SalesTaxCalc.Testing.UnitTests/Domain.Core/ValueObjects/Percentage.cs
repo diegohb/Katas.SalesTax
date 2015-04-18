bf021fd53b66c7a702fcf3ce8e78b30dc5fc96a3
bustamd1 <bustamd1@.(none)>
2015-04-17 20:49:54 -0400

    -implemented ctor for integer. -add documentation comments

--- src/SalesTaxCalc.Testing.UnitTests/Domain.Core/ValueObjects/Percentage.cs
+++ src/SalesTaxCalc.Testing.UnitTests/Domain.Core/ValueObjects/Percentage.cs
@@ -1,18 +1,23 @@
 ﻿// *************************************************
 // SalesTaxCalc.Testing.UnitTests.Percentage.cs
-// Last Modified: 04/17/2015 8:36 PM
+// Last Modified: 04/17/2015 8:44 PM
 // Modified By: Bustamante, Diego (bustamd1)
 // *************************************************
 
 namespace SalesTaxCalc.Testing.UnitTests.Domain.Core.ValueObjects
 {
-    using System;
-
+    /// <summary>
+    /// Value type that represents a percent value of 100.
+    /// </summary>
     public class Percentage : IValueObject
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Percentage"/> class.
+        /// </summary>
+        /// <param name="pValue">The integer value.</param>
         public Percentage(int pValue)
         {
-            throw new NotImplementedException();
+            Value = pValue;
         }
 
         public decimal Value { get; set; }

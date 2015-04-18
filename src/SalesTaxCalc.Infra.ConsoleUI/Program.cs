d89224e1c14fedd7d5789d3bac2092b31a0b8fc7
bustamd1 <bustamd1@.(none)>
2015-04-18 12:53:23 -0400

    implemented start menu

--- src/SalesTaxCalc.Infra.ConsoleUI/Program.cs
+++ src/SalesTaxCalc.Infra.ConsoleUI/Program.cs
@@ -6,8 +6,29 @@
 
 namespace SalesTaxCalc.Infra.ConsoleUI
 {
+    using System;
+    using System.Collections.Generic;
+    using Data;
+
     internal class Program
     {
-        private static void Main(string[] pArguments) {}
+        private static void Main(string[] pArguments)
+        {
+            var products = new List<Product>()
+            {
+                new Product {Name = "book", ProductType = ProductTypeEnum.Book, ShelfPrice = 12.49m},
+                new Product {Name = "music CD", ProductType = ProductTypeEnum.Other, ShelfPrice = 12.49m}
+            };
+
+            var command = "";
+
+            while (!command.ToLower().Equals("x"))
+            {
+                Console.WriteLine("[LP] List products");
+                Console.WriteLine("[X] Quit");
+
+                command = Console.ReadLine();
+            }
+        }
     }
 }

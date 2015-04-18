6b0b88f3d5a6d66f24bb2f7d78c29376bb2d577d
bustamd1 <bustamd1@.(none)>
2015-04-18 12:45:37 -0400

    -created initial domain classes

---
+++ src/SalesTaxCalc.Infra.ConsoleUI/Data/Product.cs
@@ -0,0 +1,10 @@
+﻿// *************************************************
+// SalesTaxCalc.Infra.ConsoleUI.Product.cs
+// Last Modified: 04/18/2015 12:44 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace SalesTaxCalc.Infra.ConsoleUI.Data
+{
+    public class Product {}
+}
---
+++ src/SalesTaxCalc.Infra.ConsoleUI/Data/ProductTypeEnum.cs
@@ -0,0 +1,11 @@
+﻿// *************************************************
+// SalesTaxCalc.Infra.ConsoleUI.ProductTypeEnum.cs
+// Last Modified: 04/18/2015 12:44 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace SalesTaxCalc.Infra.ConsoleUI.Data
+{
+    public enum ProductTypeEnum
+    {}
+}

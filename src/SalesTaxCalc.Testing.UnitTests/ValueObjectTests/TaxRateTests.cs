9a2265b3f508292d770eacad10bf1489f07c1c77
bustamd1 <bustamd1@.(none)>
2015-04-17 20:10:54 -0400

    add shell for TaxRate tests fixture

---
+++ src/SalesTaxCalc.Testing.UnitTests/ValueObjectTests/TaxRateTests.cs
@@ -0,0 +1,24 @@
+﻿// *************************************************
+// SalesTaxCalc.Testing.UnitTests.TaxRateTests.cs
+// Last Modified: 04/17/2015 8:06 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace SalesTaxCalc.Testing.UnitTests.ValueObjectTests
+{
+    using System.Diagnostics.CodeAnalysis;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TaxRateTests
+    {
+        [Test]
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public void BasicSalesTax_ShouldBeTenPercent()
+        {
+            //arrange
+            //act
+            //assert
+        }
+    }
+}

9492406216665955697d7a71bd2e0cc2ca7fa96a
bustamd1 <bustamd1@.(none)>
2015-04-17 20:30:35 -0400

    -add percentagetest fixture -add initial test

---
+++ src/SalesTaxCalc.Testing.UnitTests/ValueObjectTests/PercentageTests.cs
@@ -0,0 +1,24 @@
+﻿// *************************************************
+// SalesTaxCalc.Testing.UnitTests.PercentageTests.cs
+// Last Modified: 04/17/2015 8:25 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace SalesTaxCalc.Testing.UnitTests.ValueObjectTests
+{
+    using System.Diagnostics.CodeAnalysis;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PercentageTests
+    {
+        [Test]
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public void TenPercent_ShouldEqualOneTenth()
+        {
+            //arrange
+            //act
+            //assert
+        }
+    }
+}

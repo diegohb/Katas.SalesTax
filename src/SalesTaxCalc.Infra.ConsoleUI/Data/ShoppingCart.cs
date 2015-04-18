33483aacb1b3fbc2eff22cbe59332d3506ff12b2
bustamd1 <bustamd1@.(none)>
2015-04-18 13:41:49 -0400

    created shopping cart

---
+++ src/SalesTaxCalc.Infra.ConsoleUI/Data/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿// *************************************************
+// SalesTaxCalc.Infra.ConsoleUI.ShoppingCart.cs
+// Last Modified: 04/18/2015 1:41 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace SalesTaxCalc.Infra.ConsoleUI.Data
+{
+    using System.Collections.Generic;
+
+    public class ShoppingCart
+    {
+        private IList<Product> _items;
+
+        #region Constructors
+
+        public ShoppingCart()
+        {
+            _items = new List<Product>();
+        }
+
+        public ShoppingCart(IEnumerable<Product> pItems)
+        {
+            _items = new List<Product>(pItems);
+        }
+
+        #endregion
+    }
+}

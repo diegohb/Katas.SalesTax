094dd9496c1ad67e4e8542e3d7d9a025c0df04e2
bustamd1 <bustamd1@.(none)>
2015-04-20 19:59:37 -0400

    -created test for product pricer -created stubs for product pricing service.

---
+++ src/SalesTaxCalc.Domain.Core/Impl/ProductPricingService.cs
@@ -0,0 +1,20 @@
+﻿// *************************************************
+// SalesTaxCalc.Domain.Core.ProductPricingService.cs
+// Last Modified: 04/20/2015 7:57 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace SalesTaxCalc.Domain.Core.Impl
+{
+    using Services;
+
+    public class ProductPricingService : IPriceProduct
+    {
+        private IProvideTaxRateForProduct _taxService;
+
+        public ProductPricingService(IProvideTaxRateForProduct pTaxAssessor)
+        {
+            _taxService = pTaxAssessor;
+        }
+    }
+}
---
+++ src/SalesTaxCalc.Domain.Core/Services/IPriceProduct.cs
@@ -0,0 +1,10 @@
+﻿// *************************************************
+// SalesTaxCalc.Domain.Core.IPriceProduct.cs
+// Last Modified: 04/20/2015 7:57 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace SalesTaxCalc.Domain.Core.Services
+{
+    public interface IPriceProduct {}
+}

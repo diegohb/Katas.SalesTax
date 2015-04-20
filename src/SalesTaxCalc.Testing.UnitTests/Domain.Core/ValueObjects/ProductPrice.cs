3c633934d732f1572281b12d990895940e6bddad
bustamd1 <bustamd1@.(none)>
2015-04-20 09:39:35 -0400

    -implemented producPrice ctor -implemented taxAmount calc

--- src/SalesTaxCalc.Testing.UnitTests/Domain.Core/ValueObjects/ProductPrice.cs
+++ src/SalesTaxCalc.Testing.UnitTests/Domain.Core/ValueObjects/ProductPrice.cs
@@ -1,20 +1,25 @@
 ﻿// *************************************************
 // SalesTaxCalc.Testing.UnitTests.ProductPrice.cs
-// Last Modified: 04/20/2015 9:37 AM
+// Last Modified: 04/20/2015 9:38 AM
 // Modified By: Bustamante, Diego (bustamd1)
 // *************************************************
 
 namespace SalesTaxCalc.Testing.UnitTests.Domain.Core.ValueObjects
 {
-    using System;
-
     public class ProductPrice : IValueObject
     {
+        private readonly decimal _basePrice;
+        private readonly decimal _taxRate;
+
         public ProductPrice(decimal pBasePrice, decimal pTaxRate)
         {
-            throw new NotImplementedException();
+            _basePrice = pBasePrice;
+            _taxRate = pTaxRate;
         }
 
-        public decimal TaxAmount { get; private set; }
+        public decimal TaxAmount
+        {
+            get { return _basePrice*_taxRate; }
+        }
     }
 }

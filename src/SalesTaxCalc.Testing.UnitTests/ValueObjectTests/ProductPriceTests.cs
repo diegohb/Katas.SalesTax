0d6c8d2c00f1b8785a294d6e51aa06524ce3fc64
bustamd1 <bustamd1@.(none)>
2015-04-20 09:36:03 -0400

    -added productPrice tests fixture -added test for tax amount calculation. [not compiling]

---
+++ src/SalesTaxCalc.Testing.UnitTests/ValueObjectTests/ProductPriceTests.cs
@@ -0,0 +1,31 @@
+﻿// *************************************************
+// SalesTaxCalc.Testing.UnitTests.ProductPriceTests.cs
+// Last Modified: 04/20/2015 9:35 AM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace SalesTaxCalc.Testing.UnitTests.ValueObjectTests
+{
+    using System.Diagnostics.CodeAnalysis;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ProductPriceTests
+    {
+        [Test]
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public void ProductPrice_TaxAmountShouldEquateToBasePriceMultipliedByTaxRate()
+        {
+            //ARRANGE
+            const decimal basePrice = 11.85m;
+            const decimal taxRate = 0.15m;
+            const decimal expectedTaxAmount = basePrice*taxRate;
+
+            //ACT
+            var productPrice = new ProductPrice(basePrice, taxRate);
+
+            //ASSERT
+            Assert.AreEqual(expectedTaxAmount, productPrice.TaxAmount);
+        }
+    }
+}
